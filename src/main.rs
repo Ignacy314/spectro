76bb285b0d5203b935e4e2be453fcb48fe9c2f67
IgnacyKowalewski <ignacykowalewski314@gmail.com>
2025-04-03 16:12:07 +0200

    x

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ fn main() {
         "/home/test/mnt/dane/29-03-25_2/combined/D4_29032025.wav",
         "/home/test/mnt/dane/29-03-25_2/rust_csvs/D4_29032025.csv",
     );
-    wav - to_csv(
+    wav_to_csv(
         "/home/test/mnt/dane/29-03-25_2/combined/B4_29032025.wav",
         "/home/test/mnt/dane/29-03-25_2/rust_csvs/B4_29032025.csv",
     );
