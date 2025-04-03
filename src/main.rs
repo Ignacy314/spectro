8089a7e4d2e3a994eb1861e0720b29385b97ef33
IgnacyKowalewski <ignacykowalewski314@gmail.com>
2025-04-03 16:11:45 +0200

    x

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,17 @@ use spectro::{train_model, wav_to_csv};
 
 fn main() {
     wav_to_csv(
-        "/home/test/mnt/dane/29_03_25_2/combined/D4_29032025.wav",
-        "/home/test/mnt/dane/29_03_25_2/rust_csvs/D4_29032025.csv",
+        "/home/test/mnt/dane/29-03-25_2/combined/D4_29032025.wav",
+        "/home/test/mnt/dane/29-03-25_2/rust_csvs/D4_29032025.csv",
     );
-    wav_to_csv(
-        "/home/test/mnt/dane/29_03_25_2/combined/B4_29032025.wav",
-        "/home/test/mnt/dane/29_03_25_2/rust_csvs/B4_29032025.csv",
+    wav - to_csv(
+        "/home/test/mnt/dane/29-03-25_2/combined/B4_29032025.wav",
+        "/home/test/mnt/dane/29-03-25_2/rust_csvs/B4_29032025.csv",
     );
 
     let _model = train_model(
-        "/home/test/mnt/dane/29_03_25_2/rust_csvs/D4_29032025.csv",
-        "/home/test/mnt/dane/29_03_25_2/rust_csvs/D4_29032025.csv",
-        "/home/test/mnt/dane/29_03_25_2/rust_csvs/DB4_29032025.model",
+        "/home/test/mnt/dane/29-03-25_2/rust_csvs/D4_29032025.csv",
+        "/home/test/mnt/dane/29-03-25_2/rust_csvs/D4_29032025.csv",
+        "/home/test/mnt/dane/29-03-25_2/rust_csvs/DB4_29032025.model",
     );
 }
