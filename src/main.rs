ad3befe7fc65a1a4b01fe68780f7231846790958
IgnacyKowalewski <ignacykowalewski314@gmail.com>
2025-04-03 17:49:19 +0200

    x

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 use spectro::{train_model, wav_to_csv};
 
 fn main() {
-    // wav_to_csv(
-    //     "/home/test/mnt/dane/29-03-25_2/combined/D4_29032025.wav",
-    //     "/home/test/mnt/dane/29-03-25_2/rust_csvs/D4_29032025.csv",
-    // );
-    // wav_to_csv(
-    //     "/home/test/mnt/dane/29-03-25_2/combined/B4_29032025.wav",
-    //     "/home/test/mnt/dane/29-03-25_2/rust_csvs/B4_29032025.csv",
-    // );
+    wav_to_csv(
+        "/home/test/mnt/dane/29-03-25_2/combined/D4_29032025.wav",
+        "/home/test/mnt/dane/29-03-25_2/rust_csvs/D4_29032025.csv",
+    );
+    wav_to_csv(
+        "/home/test/mnt/dane/29-03-25_2/combined/B4_29032025.wav",
+        "/home/test/mnt/dane/29-03-25_2/rust_csvs/B4_29032025.csv",
+    );
 
     let _model = train_model(
         "/home/test/mnt/dane/29-03-25_2/rust_csvs/D4_29032025.csv",
