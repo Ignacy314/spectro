03dcc7612abf948269f07a06fe06ad938e1cf922
IgnacyKowalewski <ignacykowalewski314@gmail.com>
2025-04-03 17:48:59 +0200

    x

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ fn main() {
 
     let _model = train_model(
         "/home/test/mnt/dane/29-03-25_2/rust_csvs/D4_29032025.csv",
-        "/home/test/mnt/dane/29-03-25_2/rust_csvs/D4_29032025.csv",
+        "/home/test/mnt/dane/29-03-25_2/rust_csvs/B4_29032025.csv",
         "/home/test/mnt/dane/29-03-25_2/rust_csvs/DB4_29032025.model",
     );
 }
