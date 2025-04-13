86b4b3195d64405310776af1b320709ebfe3c176
IgnacyKowalewski <ignacykowalewski314@gmail.com>
2025-04-13 15:09:02 +0200

    x

--- random_forest.py
+++ random_forest.py
@@ -17,7 +17,9 @@ print(df)
 X = df.iloc[:, 1:]
 y = df.iloc[:, 0]
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
 print(X_train)
 print(y_test)
 
