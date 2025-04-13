ae82a7413d3da024815f8fe4dbe126b0e5ccb2ed
IgnacyKowalewski <ignacykowalewski314@gmail.com>
2025-04-13 12:59:02 +0200

    x

--- random_forest.py
+++ random_forest.py
@@ -31,10 +31,7 @@ rf.fit(X_train, y_train)
 onx = skl2onnx.convert_sklearn(
     rf,
     initial_types=[("input", FloatTensorType([None, 682]))],
-    final_types=[
-        ("label", Int64TensorType([None, 1])),
-        ("output", FloatTensorType([None, 1])),
-    ],
+    final_types=[("variable", FloatTensorType([None, 1]))],
     options={"zipmap": False},
 )
 # onx = to_onnx(rf, X[:1].astype(np.float32), options={"zipmap": False})
