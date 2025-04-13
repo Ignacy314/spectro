a14ccbe876ce376aab9bad819d0f2a1ec7861c9c
IgnacyKowalewski <ignacykowalewski314@gmail.com>
2025-04-13 19:33:09 +0200

    x

--- location.py
+++ location.py
@@ -19,7 +19,7 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-rf = RandomForestRegressor(n_estimators=16, random_state=42, n_jobs=-1, verbose=1)
+rf = RandomForestRegressor(n_estimators=64, random_state=42, n_jobs=-1, verbose=1)
 rf.fit(X_train, y_train)
 
 y_pred = rf.predict(X_test)
