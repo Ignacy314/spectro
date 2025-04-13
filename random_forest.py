31e6c191ac3b05088da2f6a37badb281a05cab69
IgnacyKowalewski <ignacykowalewski314@gmail.com>
2025-04-13 12:21:15 +0200

    x

---
+++ random_forest.py
@@ -0,0 +1,29 @@
+import sys
+
+import numpy as np
+import pandas as pd
+from skl2onnx import to_onnx
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.model_selection import train_test_split
+
+data_csvs = sys.argv[1:-1]
+dfs = [pd.read_csv(csv) for csv in data_csvs]
+
+X = pd.concat([df.iloc[:, 1:] for df in dfs], axis=0, ignore_index=True)
+y = pd.concat([df.iloc[:, 1] for df in dfs], axis=0, ignore_index=True)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+rf = RandomForestRegressor()
+rf.fit(X_train, y_train)
+
+y_pred = rf.predict(X_test)
+
+mse = mean_squared_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+print(f"MSE: {mse} | R2: {r2}")
+
+onx = to_onnx(rf, X[:1].astype(np.float32), options={"zipmap": False})
+with open(sys.argv[-1], "wb") as f:
+    f.write(onx.SerializeToString())
