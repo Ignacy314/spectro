eb496c916ae0ea552a5dc0cd826d6826fc758090
IgnacyKowalewski <ignacykowalewski314@gmail.com>
2025-05-01 15:36:39 +0200

    x

--- location.py
+++ location.py
@@ -4,7 +4,7 @@ import pandas as pd
 from skl2onnx import to_onnx
 from skl2onnx.common.data_types import DoubleTensorType, FloatTensorType
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 from sklearn.model_selection import train_test_split
 
 data_csvs = sys.argv[1:-1]
@@ -25,8 +25,9 @@ rf.fit(X_train, y_train)
 y_pred = rf.predict(X_test)
 
 mse = mean_squared_error(y_test, y_pred)
+mae = mean_absolute_error(y_test, y_pred)
 r2 = r2_score(y_test, y_pred)
-print(f"MSE: {mse} | R2: {r2}")
+print(f"MAE: {mae} | MSE: {mse} | R2: {r2}")
 
 # onx = skl2onnx.convert_sklearn(
 #     rf,
