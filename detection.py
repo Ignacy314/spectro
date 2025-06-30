e53dcfaf1eaa758b9346d6ba82a2cef2e857e7d4
IgnacyKowalewski <ignacykowalewski314@gmail.com>
2025-06-30 13:48:14 +0200

    x

--- detection.py
+++ detection.py
@@ -30,6 +30,8 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
+print(X_train)
+
 rf = RandomForestClassifier(n_estimators=4, random_state=42, n_jobs=-1, verbose=1)
 rf.fit(X_train, y_train)
 
